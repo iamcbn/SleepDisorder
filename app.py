5176d20dc6bbc60b09d2d92e4dbd214e2a5ec142
Bruno Nwagbo <nwagbob@gmail.com>
2025-06-04 20:34:21 +0100

    Logo path updated

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 import streamlit as st
+import sys, os
+
+# Setting directory to be parent root directory
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from config import LOGO_IMAGE
+
 
 # --- Define Pages ---
 home_page = st.Page("pages/home.py", title="Home")
@@ -16,7 +23,7 @@ pages = {
 navigation = st.navigation(pages)
 
 # --- App Add-ins ---
-st.logo("data/logo.png", size= "large")
+st.logo(LOGO_IMAGE, size= "large")
 st.sidebar.text("Made with ❤ by Bruno")
 st.sidebar.markdown("[![LinkedIn](https://img.shields.io/badge/LinkedIn-0077B5?style=flat&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/bruno-nwagbo)")
 st.sidebar.markdown("[![GitHub](https://img.shields.io/badge/GitHub-181717?style=flat&logo=github&logoColor=white)](https://github.com/iamcbn)")
